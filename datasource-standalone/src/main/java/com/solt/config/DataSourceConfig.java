39a5fab1f98d46d292d64a506246046ed5d82c1a
Sithuss <pla243331@gmail.com>
2023-03-06 15:21:55 +0630

    spring-data

---
+++ datasource-standalone/src/main/java/com/solt/config/DataSourceConfig.java
@@ -0,0 +1,24 @@
+package com.solt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().
+                addScript("schema.sql").
+                addScript("data.sql").setScriptEncoding("UTF-8").generateUniqueName(true).build();
+    }
+}
